ed722add860a2d3788717d1205f28a802b82269f
Chirag Gada <chiraggada2911@users.noreply.github.com>
2019-11-02 10:16:53 +0530

    absolutely new update in my website

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+  $("[rel='tooltip']").tooltip();
+});
+
+$("#menu-close").click(function(e) {
+  e.preventDefault();
+  $("#sidebar-wrapper").toggleClass("active");
+});
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#sidebar-wrapper").toggleClass("active");
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+      || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(window).scroll(function() {
+  $('.toSlideRight').each(function(){
+    var itemPos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop();
+
+    if (itemPos < topOfWindow+600) {
+      $(this).addClass("slideRight");
+    }
+  });
+
+  $('.toSlideDown').each(function(){
+    var itemPos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop();
+
+    if (itemPos < topOfWindow+600) {
+      $(this).addClass("slideDown");
+    }
+  });
+});
